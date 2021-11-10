619718c6a1f10920bcede3e4fdfe8d14bc43bf8a
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2021-11-10 17:10:05 +0100

    Update repro case

--- README.md
+++ README.md
@@ -3,5 +3,11 @@
 To reproduce:
 
 ```
-% nix-build
+# Use old naersk to build documentation
+% nix-build -A hello-good.doc
+
+# Use newnaersk to build documentation
+% nix-build -A hello-bad.doc
+... fails ...
+
 ```
--- default.nix
+++ default.nix
@@ -2,9 +2,15 @@ let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs {};
   naersk = pkgs.callPackage sources.naersk { };
+  naersk-working = pkgs.callPackage sources.naersk-working { };
 in {
 
-  hello = naersk.buildPackage {
+  hello-good = naersk-working.buildPackage {
+    root = ./hello;
+    doDoc = true;
+  };
+
+  hello-bad = naersk.buildPackage {
     root = ./hello;
     doDoc = true;
   };
